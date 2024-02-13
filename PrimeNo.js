4bf7b7f319e30533b51e38698463cda7b1449df5
Ankit Lodhi <134636170+AnkitLodhi2002@users.noreply.github.com>
2024-02-13 21:05:27 +0530

    Prime_No code

---
+++ PrimeNo.js
@@ -0,0 +1,12 @@
+//Prime Problem
+let num = 5;
+var sum = 0;
+    for(let i=1; i<=num; i++){
+        if(num%i==0)
+        num++;
+    }
+    if(sum==2){
+        console.log("Yes")
+    }else{
+        console.log("No")
+    }
